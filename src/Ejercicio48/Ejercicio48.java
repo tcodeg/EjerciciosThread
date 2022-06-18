13ceb75422e519cb46e7ced9bcd9697e62fdb30f
Tomás <Toms@DESKTOP-UEHN76G>
2022-06-18 17:48:04 +0200

    .

--- src/Ejercicio48/Ejercicio48.java
+++ src/Ejercicio48/Ejercicio48.java
@@ -1,10 +1,23 @@
 package Ejercicio48;
 
+import java.time.LocalTime;
+import java.util.function.Function;
+
 public class Ejercicio48 {
 
 	public static void main(String[] args) {
 		// FUNCTION Y BIFUNCTION
-
+		
+		System.out.println("/*****************FUNCTION PASAMOS LOCALTIME Y DEVUELVE STRING");
+		
+		Function<LocalTime,String> tiempo = LocalTime -> LocalTime.now().toString();
+		String hora = tiempo.apply(LocalTime.now());
+		System.out.println("\n"+hora);
+		System.out.println("\n\n/*Crea una expresión Function que devuelva un valor aleatorio entre 1 y un valor introducido por parámetro.*/");
+		
+		System.out.println("\n\n/*Crea una expresión Function que reciba una cadena y la devuelva convertida en mayúsculas.*/");
+		
+		
 	}
-
 }
+
